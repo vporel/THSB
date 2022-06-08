63e812e4be637693404966a217f192b0584ee1ee
synthia237 <106159104+synthia237@users.noreply.github.com>
2022-06-08 14:45:25 +0100

    modif

--- includes/head.php
+++ includes/head.php
@@ -6,7 +6,7 @@ session_start();
 const ROOT = __DIR__."/..";
 require_once ROOT."/configuration/config.php";
 require_once ROOT."/biblio/biblio.php";
-if(!isset($_INSTALLATION["etape"]) or $_INSTALLATION["etape"] < 5){
+if(!isset($_INSTALLATION["etape"]) or $_INSTALLATION["etape"] < 6){
     header("Location:admin/installation.php");
     exit();
 }
